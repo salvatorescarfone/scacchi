4acb1794c3d2b2032ec26828185e6a21e5618c17
salvatorescarfone <34183712+salvatorescarfone@users.noreply.github.com>
2018-01-08 13:17:42 +0100

    Aggiunti controlli
    Aggiunti controlli pedine dello stesso colore e doppio click sulla stessa pedina

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 var sel = false;
-var firstCell;
-var ch;
+var firstCell, secondCell;
 
-var url = window.location.href; //"file:///E:/1.Scacchi/code/index.html
-ch = url.search("code/")+5;
-url = url.substr(0,ch)+"img/";
+//controllo dinamico immagine
+
+function getUrl(x){
+  var ch = x.search("code/")+5;
+  return (x.substr(0,ch)+"img/");
+}
+
+var url = getUrl(window.location.href); //"file:///E:/1.Scacchi/code/index.html
+
+
+var Piece = function(){
+
+
+
+}
 
 function select(x,y){
-  //alert(x.toString()+","+y.toString());
 
   if(!sel){
     //cella selezionata, aspettare secondo click
     firstCell = document.getElementById(x.toString()+y.toString()).firstChild;
 
-    if(firstCell.src != url+"vuota.png")
+    if(firstCell.src != url+"vuota.png"){
       sel = true;
+      //document.getElementById(x.toString()+y.toString()).style.backgroundColor = "yellow";
+    }
+
   }
   else{
     //Secondo click, spostare la pedina
     sel = false;
-    document.getElementById(x.toString()+y.toString()).firstChild.src = firstCell.src;
-    firstCell.src = "img/vuota.png";
+    secondCell= document.getElementById(x.toString()+y.toString()).firstChild;
+
+
+    if(firstCell.src.search("b.png")!=-1){//Se la prima cella è bianca
+      if(secondCell.src.search("b.png")==-1){// e la seconda è nera
+        secondCell.src = firstCell.src;
+        firstCell.src = "img/vuota.png";
+      }
+    }
+    else{
+      //visto che la prima è nera
+      if(secondCell.src.search("n.png")==-1){// e la seconda è bianca
+        secondCell.src = firstCell.src;
+        firstCell.src = "img/vuota.png";
+      }
+    }
+
   }
 
 }
