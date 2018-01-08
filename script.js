5be8646ab87e28b12ae199025064f9bb01fd4440
salvatorescarfone <34183712+salvatorescarfone@users.noreply.github.com>
2018-01-08 22:58:43 +0100

    Aggiunti controlli
    controllo deselezione stessa pedina
    controllo turni

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var sel = false;
+var turn = "b";
 var firstCell, secondCell;
 
 //controllo dinamico immagine
-
 function getUrl(x){
   var ch = x.search("code/")+5;
   return (x.substr(0,ch)+"img/");
@@ -11,7 +11,7 @@ function getUrl(x){
 var url = getUrl(window.location.href); //"file:///E:/1.Scacchi/code/index.html
 
 
-var Piece = function(){
+function Piece(path){
 
 
 
@@ -23,22 +23,44 @@ function select(x,y){
     //cella selezionata, aspettare secondo click
     firstCell = document.getElementById(x.toString()+y.toString()).firstChild;
 
-    if(firstCell.src != url+"vuota.png"){
-      sel = true;
-      //document.getElementById(x.toString()+y.toString()).style.backgroundColor = "yellow";
+    //se è bianca ed è il turno dei bianchi
+    if(firstCell.src.search("b.png") != -1 && turn == "b"){
+
+      if(firstCell.src != url+"vuota.png"){
+        sel = true;
+        turn = "n";
+        firstCell.parentElement.style.boxShadow ="inset 0px -1px 19px 9px rgba(250,236,85,1)";
+      }
+
+    }//se è nera ed è il turno dei neri
+    else if(firstCell.src.search("n.png") != -1 && turn == "n"){
+
+      if(firstCell.src != url+"vuota.png"){
+        sel = true;
+        turn = "b";
+        firstCell.parentElement.style.boxShadow ="inset 0px -1px 19px 9px rgba(250,236,85,1)";
+      }
+
     }
 
   }
   else{
     //Secondo click, spostare la pedina
-    sel = false;
+
     secondCell= document.getElementById(x.toString()+y.toString()).firstChild;
 
+    //controllo deselezione di una stessa pedina
+    if(firstCell.src == secondCell.src){
+      firstCell.parentElement.style.boxShadow = "";
+      sel = false;
+    }
 
     if(firstCell.src.search("b.png")!=-1){//Se la prima cella è bianca
       if(secondCell.src.search("b.png")==-1){// e la seconda è nera
         secondCell.src = firstCell.src;
         firstCell.src = "img/vuota.png";
+        firstCell.parentElement.style.boxShadow = "";
+        sel = false;
       }
     }
     else{
@@ -46,9 +68,11 @@ function select(x,y){
       if(secondCell.src.search("n.png")==-1){// e la seconda è bianca
         secondCell.src = firstCell.src;
         firstCell.src = "img/vuota.png";
+        firstCell.parentElement.style.boxShadow = "";
+        sel = false;
       }
     }
-
   }
+
 
 }
